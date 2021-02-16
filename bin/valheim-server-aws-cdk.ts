3681618efba6b6d091e67905de84a4d3d3c9d839
rileydakota <dakotariley2@gmail.com>
2021-02-15 20:54:11 -0500

    fix: style

--- bin/valheim-server-aws-cdk.ts
+++ bin/valheim-server-aws-cdk.ts
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { ValheimServerAwsCdkStack } from '../lib/valheim-server-aws-cdk-stack';
-
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { ValheimServerAwsCdkStack } from "../lib/valheim-server-aws-cdk-stack";
 
 const app = new cdk.App();
-new ValheimServerAwsCdkStack(app, 'ValheimServerAwsCdkStack');
-
+new ValheimServerAwsCdkStack(app, "ValheimServerAwsCdkStack");
--- test/valheim-server-aws-cdk.test.ts
+++ test/valheim-server-aws-cdk.test.ts
@@ -1,13 +1,25 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as ValheimServerAwsCdk from '../lib/valheim-server-aws-cdk-stack';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as ValheimServerAwsCdk from "../lib/valheim-server-aws-cdk-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new ValheimServerAwsCdk.ValheimServerAwsCdkStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("Empty Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new ValheimServerAwsCdk.ValheimServerAwsCdkStack(
+    app,
+    "MyTestStack"
+  );
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
